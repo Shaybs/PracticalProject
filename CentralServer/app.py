475d3b234260c494115633b77439d5a5c502c914
Shaybs <ucashus@ucl.ac.uk>
2019-12-17 14:11:40 +0000

    adding changes to enable two VMs to connect with each other

--- CentralServer/app.py
+++ CentralServer/app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def test():
 	requests.post('http://localhost:5001/post-test', json={"name":"Bob"})
-	requests.post('http://localhost:5002/post-test', json={"name":"Shuaib"})
+	
+	shuaib = requests.post('http://localhost:5002/post-test', json={"Country":"Britain"})
+	if shuaib == True:
+		pass
+		#shuaib.json()
 	return "OK\n"
 
 if __name__== '__main__':
