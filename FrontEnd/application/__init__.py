65dc172671c8cb1601d08dae5a214ff353dcd3fa
Shaybs <ucashus@ucl.ac.uk>
2019-12-18 09:33:15 +0000

    Minor changes for testing

--- FrontEnd/application/__init__.py
+++ FrontEnd/application/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask, request
 import requests
+from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://"+os.getenv("USERNAME")+":"+os.getenv("PASSWORD")+"@"+os.getenv("MYSQL_URL")+"/"+os.getenv("MYSQL_DB")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 bootstrap = Bootstrap(app)
 
 from application import routes
