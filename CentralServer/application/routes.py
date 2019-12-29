aea062f42df7a0a831a448411e788f4a2b06ba52
Shaybs <ucashus@ucl.ac.uk>
2019-12-29 18:50:23 +0000

    Minor changes to assess interaction between services through docker

--- CentralServer/application/routes.py
+++ CentralServer/application/routes.py
@@ -2,16 +2,16 @@ from flask import abort, render_template, redirect, url_for, request, flash
 from application import app
 import requests
 
-@app.route('/test-5002', methods=['POST'])
+@app.route('/test-5002', methods=['GET','POST'])
 def test_account():	
-	country = requests.post('http://localhost:5002/post-test', json={"Country":"Pakistan"})
+	country = requests.post('http://account-service:5002/post-test', json={"Country":"Pakistan"})
 	if country.ok:
 		return country.json()["country"]
 	return "OK\n"
 
-@app.route('/test-5001', methods=['POST'])
+@app.route('/test-5001', methods=['GET','POST'])
 def test_country():
-	name = requests.post('http://localhost:5001/post-test', json={"name":"Bob"})
+	name = requests.post('http://country-service:5001/post-test', json={"name":"Bob"})
 	if name.ok:
 		return name.json()["name"]
 	return "OK\n"
