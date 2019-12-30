25abbda38c0ed759700524f0ad23134d5bcc82bb
Shaybs <ucashus@ucl.ac.uk>
2019-12-30 21:11:56 +0000

    Added tests

--- CentralServer/tests/test_requests.py
+++ CentralServer/tests/test_requests.py
@@ -1,24 +1,51 @@
 from application import app
 import unittest
 
-class TestCase(TestCase):
+class Test_Responses(TestCase):
 
-	#Test the HTTP response for the account page
-	def  test_home_view(self):
+	#Test the random number generation
+	def test_post_account_1(self):
 		response = self.client.get(url_for('test_account'))
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.count(), 1)
 
-	#Test the HTTP response for the country page
-	def  test_home_view(self):
+	#Test the random number generation
+	def test_post_account_1(self):
+		response = self.client.get(url_for('test_account_6'))
+		self.assertEqual(response.count(), 6)
+
+	#Test the random number generation
+	def test_post_account_1(self):
+		response = self.client.get(url_for('test_account_8'))
+		self.assertEqual(response.count(), 8)
+
+	#Test the random character generation
+	def test_post_account_1(self):
 		response = self.client.get(url_for('test_country'))
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.count(), 1)
 
-#One test for whether the Account number is made only integers
+	#Test the random character generation
+	def test_post_account_6(self):
+		response = self.client.get(url_for('test_country_4'))
+		self.assertEqual(response.count(), 4)
+
+	#Test the random character generation
+	def test_post_account_8(self):
+		response = self.client.get(url_for('test_country_8'))
+		self.assertEqual(response.count(), 8)
+
+	#Test IBAN generation
+	def test_iban(self):
+		response = self.client.get(url_for('test_iban'))
+		self.assertEqual(response.count(), 34)
+
+if __name__ == '__main__':
+	unittest.main()
+
 #One test for the length of Account number
 #One test for the peamble of the IBAN
 #One test for the length IBAN string
 #One test for sending information from the CentralServer to the AccountGenerator
-#One test for recieving infromation from the AccountGnerator
+#One test for recieving infromation from the AccountGenerator
 #One test for send information from the Central Server to the CountryGenerator
 #One test for recieving information from the CountryGenerator
 #One test for send information from the Central Server to the FrontEnd
--- CountryGenerator/tests/test_repsonse.py
+++ CountryGenerator/tests/test_repsonse.py
@@ -1,14 +1,35 @@
 from application import app
 import unittest
 
-class TestCase(TestCase):
+class Test_Responses(TestCase):
 
 	#Test the HTTP response for the account page
-	def  test_home_view(self):
-		response = self.client.get(url_for('test_account'))
+	def  test_post_account_view(self):
+		response = self.client.get(url_for('get_test'))
 		self.assertEqual(response.status_code, 200)
 
-	#Test the HTTP response for the country page
-	def  test_home_view(self):
-		response = self.client.get(url_for('test_country'))
-		self.assertEqual(response.status_code, 200)
+	#Test the random number generation
+	def test_post_account_1(self):
+		response = self.client.get(url_for('post_test'))
+		self.assertEqual(response.count(), 1)
+
+	#Test the random number generation
+	def test_post_account_6(self):
+		response = self.client.get(url_for('post_test_4'))
+		self.assertEqual(response.count(), 4)
+
+	#Test the random number generation
+	def test_post_account_8(self):
+		response = self.client.get(url_for('post_test_8'))
+		self.assertEqual(response.count(), 8)
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+#Is it successfully connecting to the central server
+#Is it recieving an object
+#Is it generating a pre-amble for the IBAN
+#Is it generating 4 random characters?
+#Is it generating 8 random characters
+#Are the characters being sent to the central server?
