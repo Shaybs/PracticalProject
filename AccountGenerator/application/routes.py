01366f9bc66a7cc31e494eea48639454c61f1375
Shaybs <ucashus@ucl.ac.uk>
2019-12-31 19:42:24 +0000

    Adding the ability to generate an account with 8 numbers

--- AccountGenerator/application/routes.py
+++ AccountGenerator/application/routes.py
@@ -7,7 +7,7 @@ import random
 def get_test():
 	return {"name" : "Bob"}
 
-@app.route('/post-account', methods=['POST'])
+@app.route('/post-account-6', methods=['POST'])
 def post_test():
 	country = request.get_json()["Country"]
 	if country == "Pakistan":
@@ -21,4 +21,304 @@ def post_test():
 		value4 = random.randint(1,9)
 		value4_string = str(value4)
 		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "Belarus":
+		initial = "61"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "United Kingdom":
+		initial = "62"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "United Arab Emirates":
+		initial = "63"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "South Korea":
+		initial = "64"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "Italy":
+		initial = "65"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "China":
+		initial = "66"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "India":
+		initial = "67"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "Singapore":
+		initial = "67"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "Denmark":
+		initial = "68"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+	elif country == "Switzerland":
+		initial = "69"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string
+		return {"Account":response_value}
+
+@app.route('/post-account-8', methods=['POST'])
+def post_test():
+	country = request.get_json()["Country"]
+	if country == "Pakistan":
+		initial = "60"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "Belarus":
+		initial = "61"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "United Kingdom":
+		initial = "62"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "United Arab Emirates":
+		initial = "63"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "South Korea":
+		initial = "64"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "Italy":
+		initial = "65"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "China":
+		initial = "66"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "India":
+		initial = "67"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "Singapore":
+		initial = "67"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "Denmark":
+		initial = "68"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
+		return {"Account":response_value}
+	elif country == "Switzerland":
+		initial = "69"
+		value1 = random.randint(1,9)
+		value1_string = str(value1)
+		value2 = random.randint(1,9)
+		value2_string = str(value2)
+		value3 = random.randint(1,9)
+		value3_string = str(value3)
+		value4 = random.randint(1,9)
+		value4_string = str(value4)
+		value5 = random.randint(1,9)
+		value5_string = str(value5)
+		value6 = random.randint(1,9)
+		value6_string = str(value6)
+		response_value = initial + value1_string + value2_string + value3_string + value4_string + value5_string + value6_string
 		return {"Account":response_value}
--- CentralServer/application/routes.py
+++ CentralServer/application/routes.py
@@ -2,9 +2,16 @@ from flask import Flask, request
 from application import app
 import requests
 
-@app.route('/test-5002', methods=['GET','POST'])
+@app.route('/get_account_6', methods=['GET','POST'])
 def test_account():	
-	account = requests.post('http://account-service:5002/post-account', json={"Country":"Pakistan"})
+	account = requests.post('http://account-service:5002/post-account-6', json={"Country":"Pakistan"})
+	if account.ok:
+		return account.json()["Account"]
+	return "OK\n"
+
+@app.route('/get_account_8', methods=['GET','POST'])
+def test_account():	
+	account = requests.post('http://account-service:5002/post-account-8', json={"Country":"Pakistan"})
 	if account.ok:
 		return account.json()["Account"]
 	return "OK\n"
