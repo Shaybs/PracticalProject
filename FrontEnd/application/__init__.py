dc662815f0c9147c4319d4c6c7c78354deeb4331
Shaybs <ucashus@ucl.ac.uk>
2020-01-02 22:50:42 +0000

    Adding frontend changes

--- FrontEnd/application/__init__.py
+++ FrontEnd/application/__init__.py
@@ -8,6 +8,7 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://"+os.getenv("USERNAME")+":"+os.getenv("PASSWORD")+"@"+os.getenv("MYSQL_URL")+"/"+os.getenv("MYSQL_DB")
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 app.app_context().push()
 db.init_app(app)
