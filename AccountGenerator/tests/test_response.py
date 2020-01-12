74928457ea908f05fd5ef903e4f1b6a9f6f4b6c1
Shaybs <ucashus@ucl.ac.uk>
2020-01-12 20:15:55 +0000

    adding more tests

--- AccountGenerator/tests/test_response.py
+++ AccountGenerator/tests/test_response.py
@@ -24,14 +24,13 @@ def mocked_requests_get(*args, **kwargs):
 class ResponseTestClass(unittest.TestCase):
     @mock.patch('requests.get', side_effect=mocked_requests_get)
     def test_account(self, mock_get):
-		account6 = requests.get('http://account-service:5002/post-account-6').json()
+        account6 = requests.get('http://account-service:5002/post-account-6').json()
         self.assertEqual(account6, {"Account": "609292"})
 
     @mock.patch('requests.get', side_effect=mocked_requests_get)
     def test_account(self, mock_get):
 		account8 = requests.get('http://account-service:5002/post-account-8').json()
 		self.assertEqual(account8, {"Account": "67499382"})
-
 
 if __name__ == '__main__':
 	unittest.main()
